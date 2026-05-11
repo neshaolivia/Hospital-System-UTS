69dd4ac00a8839762a6c9b015ff8820e28133230
? <reikaelvaretta@gmail.com>
2026-05-11 17:17:08 +0700

    feat: add appointment service with RabbitMQ async communication and Docker setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8c0bef34eae815c3e3c8403320753df82f0bfcab',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8c0bef34eae815c3e3c8403320753df82f0bfcab',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
